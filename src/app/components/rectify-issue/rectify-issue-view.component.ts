948a2e220a8cbee71427cf5b98ae80b70d821844
unknown <769613912@qq.com>
2021-09-24 16:39:45 +0800

    整改问题页面修改

--- src/app/components/rectify-issue/rectify-issue-view.component.ts
+++ src/app/components/rectify-issue/rectify-issue-view.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'rectify-issue-view',
   templateUrl: './rectify-issue-view.component.html',
-  styles: []
+  styles: [],
 })
 export class RectifyIssueViewComponent implements OnInit {
+  listOfOption: string[] = ['未下发', '已下发', '未移交'];
+  listOfSelectedValue = ['未下发'];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
