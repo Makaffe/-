f6075487cb9c6163bef0b7dab57aed38a01a0c86
unknown <769613912@qq.com>
2021-09-30 17:11:54 +0800

    修改审计报告详情页面

--- src/app/components/rectify-post/rectify-post-detail.component.ts
+++ src/app/components/rectify-post/rectify-post-detail.component.ts
@@ -9,7 +9,7 @@ export class RectifyPostDetailComponent implements OnInit {
   /**
    * 左侧宽度常量
    */
-  LEFT_WIDTH = 180;
+  LEFT_WIDTH = 300;
 
   /**
    * 左侧宽度常量
@@ -23,6 +23,17 @@ export class RectifyPostDetailComponent implements OnInit {
    * 左侧组织机构树上部分高度
    */
   topSize = this.TOP_HIGHT;
+
+  /**
+   * 步骤条进度
+   */
+  current = 0;
+  /**
+   * 判断是否只读
+   */
+  readFlag1: boolean;
+  readFlag2: boolean;
+  visabled = false;
   listOfData = [
     {
       id: '1',
@@ -43,4 +54,37 @@ export class RectifyPostDetailComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+  pre(): void {
+    this.current -= 1;
+    this.changeContent();
+  }
+
+  next(): void {
+    this.current += 1;
+    this.visabled = true;
+    this.changeContent();
+  }
+
+  done(): void {
+    console.log('done');
+  }
+
+  changeContent(): void {
+    switch (this.current) {
+      case 0: {
+        this.readFlag1 = false;
+        this.readFlag2 = true;
+        break;
+      }
+      case 1: {
+        this.readFlag1 = true;
+        this.readFlag2 = false;
+        break;
+      }
+      default: {
+        this.readFlag1 = true;
+        this.readFlag1 = false;
+      }
+    }
+  }
 }
